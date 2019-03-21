73233bbdad496452c5f5f9d7125b6ab06346b80b
Javier Cases Sempere <37308043+JCases@users.noreply.github.com>
2019-03-21 16:28:57 +0100

    Revert "Revert "Create Scheame Project""
    This reverts commit bc1274abe1b2da4c7dd1ad7cb635574c9c798d21.

---
+++ src/components/PopUp.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export class PopUp extends Component<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            visible: false,
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
